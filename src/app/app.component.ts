e7182f7cac38cf19487a332e0dffb9812c00ed25
CodeSandbox <support@codesandbox.io>
2023-11-09 17:56:43 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { stationList } from './station-list.const';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'CodeSandbox';
+  list = stationList;
 }
